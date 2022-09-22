233b0bfe1a4a3f2685f4e4e6edc570794afde525
Jun <wnsgur4092@gmail.com>
2022-09-22 23:21:29 +1000

    Initial Commit

---
+++ TarongaZoo-SwiftUI/TarongaZoo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarongaZoo_SwiftUIApp.swift
+//  TarongaZoo-SwiftUI
+//
+//  Created by JunHyuk Lim on 22/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct TarongaZoo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
